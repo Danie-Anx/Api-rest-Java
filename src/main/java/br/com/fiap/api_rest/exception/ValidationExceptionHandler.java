b084232f63e0cf089bcbd651bfb908a4a99ac770
labsfiap <danielcoimbra.anx@gmail.com>
2025-02-24 19:48:11 -0300

    adicionando

--- src/main/java/br/com/fiap/api_rest/exception/ValidationExceptionHandler.java
+++ src/main/java/br/com/fiap/api_rest/exception/ValidationExceptionHandler.java
@@ -1,5 +1,9 @@
 package br.com.fiap.api_rest.exception;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
+@Controller
+@ControllerAdvice
 public class ValidationExceptionHandler {
 }
