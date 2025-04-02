d18e24c3989e8162764014625df0dcc2ea3c90cc
Daniel-Anx <danielcoimbra.anx@gmail.com>
2025-04-01 21:32:22 -0300

    finalizando projeto

--- src/main/java/br/com/fiap/api_rest/dto/LivroRequestDTO.java
+++ src/main/java/br/com/fiap/api_rest/dto/LivroRequestDTO.java
@@ -1,8 +1,5 @@
 package br.com.fiap.api_rest.dto;
 
-import br.com.fiap.api_rest.model.Autor;
-
-import java.util.List;
-
-public record LivroRequestDTO(String titulo, List<Autor> autores) {
+public record LivroRequestDTO(String titulo,
+                              String autor) {
 }
